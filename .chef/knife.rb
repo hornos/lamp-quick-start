1c6575c0afab9ea9656db16e16d46dc5ea620e7d
Tom Hornos <tom.hornos@gmail.com>
2012-08-22 10:25:02 +0200

    first commit

---
+++ .chef/knife.rb
@@ -0,0 +1,14 @@
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "hornos"
+client_key               "#{current_dir}/hornos.pem"
+validation_client_name   "hornos-validator"
+validation_key           "#{current_dir}/hornos-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/hornos"
+cache_type               'BasicFile'
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path            ["#{current_dir}/../cookbooks"]
+cookbook_copyright       "Tom Hornos"
+cookbook_email           "tom.hornos@gmail.com"
+cookbook_license         "apachev2"
